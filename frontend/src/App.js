89efbe4348552d3bf2fe758cd92478d36dd164ab
starkz24 <aman.kumarz2404@gmail.com>
2023-11-23 00:21:36 +0530

    css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import './App.css'
 
 function App() {
   const [questions, setQuestions] = useState([]);
-  const [totalMarks, setTotalMarks] = useState();
-  const [easyPercentage, setEasyPercentage] = useState();
-  const [mediumPercentage, setMediumPercentage] = useState();
-  const [hardPercentage, setHardPercentage] = useState();
+  const [totalMarks, setTotalMarks] = useState('');
+  const [easyPercentage, setEasyPercentage] = useState('');
+  const [mediumPercentage, setMediumPercentage] = useState('');
+  const [hardPercentage, setHardPercentage] = useState('');
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const fetchQuestions = async () => {
     try {
@@ -23,9 +24,16 @@ function App() {
         total: parseInt(totalMarks),
       };
 
+      const totalPercentage = parseInt(easyPercentage) + parseInt(mediumPercentage) + parseInt(hardPercentage);
+      if (totalPercentage !== 100) {
+        setErrorMessage('Invalid percentage distribution. The total should be 100%.');
+        setLoading(false);
+        return;
+      }
+      setErrorMessage('');
+
       const response = await axios.post('http://localhost:3001/api/questions', requestData);
       setQuestions(response.data);
-
     } catch (error) {
       console.error('Error fetching questions:', error);
     } finally {
@@ -65,6 +73,7 @@ function App() {
         <button type="submit" disabled={loading}>
           {loading ? 'Generating...' : 'Generate Questions'}
         </button>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
       </form>
       <h2>Questions:</h2>
       <ul>
